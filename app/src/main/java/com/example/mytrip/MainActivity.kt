31f4001aa99f2a66766dfa6147703e0da93ad611
José Carlos Jr <jose.jr@tap4mobile.com.br>
2019-06-12 17:11:01 -0300

    Implementando a lógica da aplicação

--- app/src/main/java/com/example/mytrip/MainActivity.kt
+++ app/src/main/java/com/example/mytrip/MainActivity.kt
@@ -2,13 +2,38 @@ package com.example.mytrip
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.buttonCalculate){
+            handleCalculate()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonCalculate.setOnClickListener(this)
 
+    }
+
+    private fun handleCalculate(){
+        if(isValid()){
+            try {
+            }catch (nfe: NumberFormatException){
+                Toast.makeText(this,getString(R.string.valores_validos_exception),Toast.LENGTH_LONG).show()
+            }
+        }else{
+            Toast.makeText(this,getString(R.string.valores_validos_exception),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isValid() : Boolean{
+        return editDistance.text.isNotEmpty() && editAutonomy.text.isNotEmpty() && editAutonomy.text.isNotEmpty() && editAutonomy.text.toString() != "0"
     }
 }
